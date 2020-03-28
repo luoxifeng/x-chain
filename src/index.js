4e309316795a06f33a39ecdcc3e4f2093c276175
Luo XiFeng <942394682@qq.com>
2020-03-28 17:13:45 +0800

    add：配置函数必须有结尾标记

--- src/index.js
+++ src/index.js
@@ -3,37 +3,47 @@
  */
 const defineProps = Object.defineProperties;
 
-const xchain = (target = {}, fileds = [], fun = () => { }) => {
 
-  function chain(applyPath) {
-    function innerChain() {
+const xChain = (target = {}, fileds = [], fun = () => { }) => {
 
+  function createChain(applyPath) {
+    function chain(...args) {
+      console.log(chain._applyPath, ...args);
     }
-    innerChain.__proto__ = proto;
-    innerChain._applyPath = applyPath;
-    return innerChain;
+
+    chain.__proto__ = proto;
+    chain._applyPath = applyPath;
+
+    return chain;
   }
 
-  const props = fileds.reduce((acc, key) => {
+  let hasFun = false;
+  let hasEnd = false;
+  const props = {};
+
+  fileds.forEach((acc, key) => {
     const isfun = /\(.*?\)/.test(key);
-    const [defaultval, ...othervalues] = (RegExp.$1 || '').split('|')
+    const [defaultValue, ...otherValues] = (RegExp.$1 || '').split('|');
+
     if (isfun) {
 
     } else {
-
-    }
-
-    acc[key] = {
-      get() {
-        return
+      props[key] = {
+        get() {
+          return createChain((this._applyPath || []).concat(key));
+        }
       }
     }
-    return acc;
-  }, {});
+  });
+
+  if (!(hasFun && hasEnd)) {
+    throw new Error('++++++');
+  }
 
   const proto = defineProps(function () { }, props)
 
-    (target, props);
 }
+
+module.exports = xChain;
 
 
